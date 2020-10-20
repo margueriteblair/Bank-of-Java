62f925e792b882d67c7da426c0c1699cab7b1ee9
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-20 17:12:53 -0400

    compound interest method

--- src/com/margieblair/SavingsAccount.java
+++ src/com/margieblair/SavingsAccount.java
@@ -1,5 +1,14 @@
 package com.margieblair;
 
-public class SavingsAccount {
+public class SavingsAccount extends BankAccount {
+    private int interest;
 
+    public SavingsAccount(int accountNumber, int balance, String owner, int interest) {
+        super(accountNumber, balance, owner, "Savings");
+        this.interest = interest;
+    }
+
+    public void compoundInterest() {
+        balance += (balance * ((float) interest / 100.0));
+    }
 }
