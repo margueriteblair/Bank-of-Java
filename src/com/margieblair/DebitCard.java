74bcdf5125f034f37058324387889f6f0eb24b0a
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-20 20:51:07 -0400

    declared fields for debit card

--- src/com/margieblair/DebitCard.java
+++ src/com/margieblair/DebitCard.java
@@ -1,4 +1,12 @@
 package com.margieblair;
 
+import java.util.*;
+
 public class DebitCard {
+    private String pin;
+    private CheckingAccount primary;
+    private String cvv;
+    private String cardOwner;
+    private String cardNumber;
+    private List<SavingsAccount> atmAccounts; //should actually make an interface
 }
