10f5fa472516fb34df86904d4a1e22a8b1f4d3a5
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-20 22:22:52 -0400

    creating client class

--- src/com/margieblair/Client.java
+++ src/com/margieblair/Client.java
@@ -1,4 +1,28 @@
 package com.margieblair;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Client {
+
+    private String name;
+    private String firstname;
+    private String lastName;
+    private String middleName;
+    private String clientId;
+    private List<CheckingAccount> checkingAccounts;
+    private List<SavingsAccount> savingsAccounts;
+    private List<CDInvestment> cdInvestments;
+
+    public Client(String firstname, String lastName, String clientId) {
+        this.name = firstname + " " + lastName;
+        this.firstname = firstname;
+        this.lastName = lastName;
+        this.clientId = clientId;
+        this.checkingAccounts = new ArrayList<CheckingAccount>();
+        this.savingsAccounts = new ArrayList<SavingsAccount>();
+        this.cdInvestments = new ArrayList<CDInvestment>();
+     }
+
+
 }
