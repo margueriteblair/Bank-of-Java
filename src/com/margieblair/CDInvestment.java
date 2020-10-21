a70447695f186348c3c9b1e42620df89100deaf2
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-20 21:26:35 -0400

    implemented CD investment as a grandchild class

--- src/com/margieblair/CDInvestment.java
+++ src/com/margieblair/CDInvestment.java
@@ -1,5 +1,24 @@
 package com.margieblair;
 
-public class CDInvestment {
+public class CDInvestment extends InvestmentAccount {
     //hello world
+    private  int timeFrame;
+    private char timePeriod = 'Y';
+
+    public CDInvestment(int balance, int accountNum, String owner, int interestRate, int period, int timeFrame) {
+        super(balance, accountNum, owner, interestRate, period, "M");
+        this.timeFrame = timeFrame;
+    }
+
+    public void checkIfInterest(int time) {
+        if (time >= timeFrame && balance > 0) {
+            applyInterest();
+        }
+    }
+
+    @Override
+    public void withdraw(int amt) {
+        return;
+    }
+
 }
