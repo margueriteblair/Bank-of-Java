cf9bab1b91eba10aa36bb3703773aa5ac1aebf71
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-21 22:18:51 -0400

    all methods we create in this interface are part of a contract

--- src/com/margieblair/Account.java
+++ src/com/margieblair/Account.java
@@ -1,9 +1,11 @@
 package com.margieblair;
 
 public interface Account {
+    //everything inside of an interface will either be public or final
+    //we're setting up a contract
     public void withdraw(int amt);
     public void deposit(int amt);
-
+    public String getOwner();
     public String getDetails(int amt);
     public int getBalance();
 }
